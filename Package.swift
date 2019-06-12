1c05a8421c34774342a2c6144e1b7b5efea21a63
Kazuya Ueoka <info@fromkk.info>
2019-06-12 21:11:08 +0900

    remove Extensions

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .target(
             name: "DifferenceKit",
             path: "Sources",
-            exclude: ["Extensions"]
+            exclude: []
         ),
         .testTarget(
             name: "DifferenceKitTests",
